4ea43c0fb012f63e745e713da2618035cd7e724d
Charlie <charlie3.thomas@live.uwe.ac.uk>
2023-03-17 22:18:48 +0000

    [GRD-ASD] Complete dialogue tree traversal. "Hello World"

--- Assets/TestSOScript.cs
+++ Assets/TestSOScript.cs
@@ -2,19 +2,25 @@ using CT.ScriptableObjects;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+using TMPro;
 
 namespace CT
 {
     using Data;
-    using TMPro;
-    using Unity.VisualScripting;
 
     public class TestSOScript : MonoBehaviour
     {
         [SerializeField] private CTDialogueSO starting_dlog;
+
         [SerializeField] private TextMeshProUGUI text_UI;
 
-        private CTDialogueSO current_dlog;
+        [SerializeField] private Button test_button_1;
+        [SerializeField] private Button test_button_2;
+        [SerializeField] private Button test_button_3;
+
+        [SerializeField] private CTDialogueSO current_dlog;
 
         private void Awake()
         {
@@ -23,24 +29,59 @@ namespace CT
 
         private void Start()
         {
+            // Buttons
+            test_button_1.onClick.AddListener(TestButton1);
+            test_button_2.onClick.AddListener(TestButton2);
+            test_button_3.onClick.AddListener(TestButton3);
+
             ShowText();
             //OnOptionChosen(0);
         }
 
         private void ShowText()
         {
-            Debug.Log("current_dlog.IsStartingDialogue : " + current_dlog.IsStartingDialogue);
-            Debug.Log("current_dlog.DialogueName : " + current_dlog.DialogueName);
-            Debug.Log("current_dlog.DialogueType : " + current_dlog.DialogueType);
-            for (int i = 0; i < current_dlog.Choices.Count; i++)
-            {
-                Debug.Log("current_dlog.Choices " + i + " : " + current_dlog.Choices[i].Text);
-            }
-            Debug.Log("current_dlog.name : " + current_dlog.name);
-            Debug.Log("current_dlog.Text : " + current_dlog.Text);
-            Debug.Log("current_dlog.TipText : " + current_dlog.TipText);
+            // Clear text from previous node
+            test_button_1.GetComponentInChildren<TextMeshProUGUI>().text = "";
+            test_button_2.GetComponentInChildren<TextMeshProUGUI>().text = "";
+            test_button_3.GetComponentInChildren<TextMeshProUGUI>().text = "";
 
+
+            //// Debug
+            //Debug.Log("current_dlog.IsStartingDialogue : " + current_dlog.IsStartingDialogue);
+            //Debug.Log("current_dlog.DialogueName : " + current_dlog.DialogueName);
+            //Debug.Log("current_dlog.DialogueType : " + current_dlog.DialogueType);
+            //for (int i = 0; i < current_dlog.Choices.Count; i++)
+            //{
+            //    Debug.Log("current_dlog.Choices " + i + " : " + current_dlog.Choices[i].Text);
+            //}
+            //Debug.Log("current_dlog.name : " + current_dlog.name);
+            //Debug.Log("current_dlog.Text : " + current_dlog.Text);
+            //Debug.Log("current_dlog.TipText : " + current_dlog.TipText);
+
+            // UI Text
             text_UI.text = current_dlog.Text;
+
+            // UI Buttons
+            // Need something more elegant but this is fine for testing purposes.
+            if (current_dlog.DialogueType == Enumerations.CTDialogueType.MultipleChoice)
+            {
+                if (current_dlog.Choices.Count > 0)
+                    test_button_1.GetComponentInChildren<TextMeshProUGUI>().text = string.IsNullOrEmpty(current_dlog.Choices[0].Text) ? "" : current_dlog.Choices[0].Text;
+
+                if (current_dlog.Choices.Count > 1)
+                    test_button_2.GetComponentInChildren<TextMeshProUGUI>().text = string.IsNullOrEmpty(current_dlog.Choices[1].Text) ? "" : current_dlog.Choices[1].Text;
+
+                if (current_dlog.Choices.Count > 2)
+                    test_button_3.GetComponentInChildren<TextMeshProUGUI>().text = string.IsNullOrEmpty(current_dlog.Choices[2].Text) ? "" : current_dlog.Choices[2].Text;
+
+            }
+            //else
+            //{
+            //    test_button_1.GetComponentInChildren<TextMeshProUGUI>().text = "";
+            //    test_button_2.GetComponentInChildren<TextMeshProUGUI>().text = "";
+            //    test_button_3.GetComponentInChildren<TextMeshProUGUI>().text = "";
+            //}
+            
         }
 
         private void OnOptionChosen(int _choice_index)
@@ -57,6 +98,21 @@ namespace CT
             current_dlog = next_dlog;
 
             ShowText();
+        }
+
+        private void TestButton1()
+        {
+            OnOptionChosen(0);
+        }
+
+        private void TestButton2()
+        {
+            OnOptionChosen(1);
+        }
+
+        private void TestButton3()
+        {
+            OnOptionChosen(2);
         }
     }
 }
