116621f98830f1a73e9834e29d67e55c83efca1b
Charlie <charlie3.thomas@live.uwe.ac.uk>
2023-04-02 22:46:39 +0100

    [GRD-ASD] Placeholder character instantiation and tip button

--- Assets/NewUI/CTUISetupUtility.cs
+++ Assets/NewUI/CTUISetupUtility.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 namespace CT
 {
@@ -49,17 +51,33 @@ namespace CT
         //[SerializeField] UnityEngine.UI.Button next_button;     
         //[SerializeField] UnityEngine.UI.Button choice_button;   
         //[SerializeField] UnityEngine.UI.Button show_tip_ui;
-        //
-        [SerializeField] GameObject character_image;
-        [SerializeField] GameObject location_image;
 
+        // Prefabs
+        [Header("UI Prefabs")]
+        [SerializeField] private GameObject image_prefab;
+        [SerializeField] private GameObject text_prefab;
+        [SerializeField] private GameObject button_prefab;
+
+        // Anchor Points
+        [Header("Anchor Points")]
+        [SerializeField] private Transform background_anchor_point;
+        [SerializeField] private Transform character_anchor_point;
+        [SerializeField] private Transform tip_anchor_point;
+
+        // UI Elements
         private GameObject background;
-
+        private GameObject character;
+        private GameObject show_tip_button;
+        private GameObject next_button;
+        private List<GameObject> choices_buttons;
 
         private void Start()
         {
             background = new GameObject();
+            character = new GameObject();
             InstantiateNewBackground(0);
+            InstantiateNewCharacter(0);
+            InstantiateRevealTipButton();
         }
 
         private void Update()
@@ -68,26 +86,102 @@ namespace CT
             {
                 int index = Random.Range(0, location_sprites.Count);
                 InstantiateNewBackground(index);
+                index = Random.Range(0, character_sprites.Count);
+                InstantiateNewCharacter(index);
             }
         }
 
+        #region Button Methods
+        private void InstantiateRevealTipButton()
+        {
+            // Instantiate tip button and place at fixed position on the screen
+            show_tip_button = Instantiate(button_prefab, tip_anchor_point);
+            show_tip_button.GetComponentInChildren<TextMeshProUGUI>().text = "";
+            ResizeButtonToTextureScale(show_tip_button.GetComponent<UnityEngine.UI.Button>());
+        }
+
+        private void InstantiateNextDialogueButton()
+        {
+            // Instantiate next button and place at fixed position on the screen
+            show_tip_button = Instantiate(button_prefab, this.transform);
+
+            ResizeButtonToTextureScale(show_tip_button.GetComponent<UnityEngine.UI.Button>());
+        }
+
+        private void InstantiateChoiceButton()
+        {
+            // Instantiate button
+            // Add button to list
+            // Set button text to choice text#
+            // Iterate though list and place buttons in a row with nice spacing
+        }
+
+        private void ResizeButtonToTextureScale(UnityEngine.UI.Button _button)
+        {
+            // Get the size of button texture
+            Texture2D texture = _button.GetComponent<UnityEngine.UI.Image>().sprite.texture;
+            Vector2 textureSize = new Vector2(texture.width, texture.height);
+
+            // Set size of button to match texture
+            RectTransform rectTransform = _button.GetComponent<RectTransform>();
+            rectTransform.sizeDelta = textureSize;
+        }
+
+        #endregion
+
+
+        #region Sprite Methods
+        #region Character Methods
+
+        private void InstantiateNewCharacter(int _index)
+        {
+            // Ensure index is within range
+            EnsureIndexWithinRange(_index, character_sprites);
+
+            Destroy(character.gameObject);
+
+            character = Instantiate(image_prefab, character_anchor_point); // Instantiate image object
+            character.GetComponent<UnityEngine.UI.Image>().sprite = ConvertTexture2DToSprite(character_sprites[_index]); // Set sprite
+            SetAnchors(character);
+        }
+
+        private void SetCharacterPosition()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Background Methods
         private void InstantiateNewBackground(int _index)
         {
             // Ensure index is within range
-            if (_index < 0 || _index > location_sprites.Count)
-            {
-                Debug.LogError("Index out of range");
-                return;
-            }
+            EnsureIndexWithinRange(_index, location_sprites);
 
             Destroy(background.gameObject);
 
-            background = Instantiate(character_image, this.transform); // Instantiate image object
-            background.GetComponent<Image>().sprite = ConvertTexture2DToSprite(location_sprites[_index]); // Set sprite
+            background = Instantiate(image_prefab, this.transform); // Instantiate image object
+            background.GetComponent<UnityEngine.UI.Image>().sprite = ConvertTexture2DToSprite(location_sprites[_index]); // Set sprite
             SetAnchors(background);
-            StretchToFillCanvas(background.GetComponent<Image>(), this.gameObject.GetComponent<Canvas>());
+            StretchToFillCanvas(background.GetComponent<UnityEngine.UI.Image>(), this.gameObject.GetComponent<Canvas>());
+            background.transform.parent = background_anchor_point;
         }
 
+        private void StretchToFillCanvas(UnityEngine.UI.Image _image, Canvas _canvas)
+        {
+            RectTransform rt = _image.GetComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0, 0); // set anchorMin to the center of the parent
+            rt.anchorMax = new Vector2(1, 1); // set anchorMax to the center of the parent
+            rt.pivot = new Vector2(0.5f, 0.5f); // set pivot to the center of the element
+            rt.sizeDelta = new Vector2(0, 0);
+            rt.anchoredPosition = new Vector2(0, 0);
+        }
+        #endregion
+        #endregion
+
+
+        #region Utility Methods
         private Sprite ConvertTexture2DToSprite(Texture2D _tex)
         {
             return Sprite.Create(_tex, new Rect(0, 0, _tex.width, _tex.height), new Vector2(0.5f, 0.5f), 100);
@@ -101,14 +195,17 @@ namespace CT
             rt.pivot = new Vector2(0.5f, 0.5f); // set pivot to the center of the element
         }
 
-        private void StretchToFillCanvas(Image _image, Canvas _canvas)
+        private bool EnsureIndexWithinRange<_T>(int _index, List<_T> _list)
         {
-            RectTransform rt = _image.GetComponent<RectTransform>();
-            rt.anchorMin = new Vector2(0, 0); // set anchorMin to the center of the parent
-            rt.anchorMax = new Vector2(1, 1); // set anchorMax to the center of the parent
-            rt.pivot = new Vector2(0.5f, 0.5f); // set pivot to the center of the element
-            rt.sizeDelta = new Vector2(0, 0);
-            rt.anchoredPosition = new Vector2(0, 0);
+            if (_index < 0 || _index > _list.Count)
+            {
+                Debug.LogError("Index out of range");
+                return false;
+            }
+
+            return true;
         }
+
+        #endregion
     }
 }
